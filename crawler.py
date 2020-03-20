3fcd967f2fffa6ba8fa5fdfb61f2a95ae4ddb37a
2288115k <2288115k@student.gla.ac.uk>
2020-03-20 09:20:07 +0000

    fixed crawler

--- crawler.py
+++ crawler.py
@@ -3,10 +3,10 @@ import tweepy
 import pymongo
 import datetime
 # authorization tokens
-consumer_key = "vOc9LolIXAJI2JW6X3u33GK7h"
-consumer_secret = "Pz2FIUI3Oy3s0HhMG6YeBfZY6Gm52ebEEEO0t4n6rPrQYILvX9"
-access_key = "728347342034894850-Y7WmUZmjBypTiyjhL96qYaTimsGYYnH"
-access_secret = "yl709OhGtdGqfPGhl1SNNHcF0yCXVROFh8MruZ53duaPo"
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
 
 
 class StreamListener(tweepy.StreamListener):
